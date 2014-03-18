ace8c3b10aa4697cc3a5fa36d67ce651bb7e3dba
BlueHotDog <dannifriedland@gmail.com>
2014-03-18 17:32:48 +0200

    Fixing gulp not running tests after change
    Added plumber to fix an error in nodejs streams.

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,24 @@ var nodemon = require('gulp-nodemon');
 var coffeelint = require('gulp-coffeelint');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
 var mocha = require('gulp-mocha');
 
 const BUILD_FOLDER = './public/build/';
 
+const PATHS = {
+  css: './app/assets/css/**',
+  tests: './test/specs',
+  client: './client',
+};
+
 gulp.task('develop', function () {
-  nodemon({ script: 'server.js', ext: 'html js coffee', ignore: ['ignored.js'] })
+  nodemon({ script: 'server.js', ext: 'html js coffee', ignore: ['ignored.js'] });
 });
 
 gulp.task('css', function(){
   var less = require('gulp-less');
-  gulp.src('./app/assets/css/**/*.less')
+  gulp.src(PATHS.css+'/*.less')
       .pipe(less())
       .pipe(concat('style.css'))
       .pipe(gulp.dest('./public/css'));
@@ -23,25 +30,22 @@ gulp.task('css', function(){
 
 gulp.task('mocha', function () {
   require('coffee-script/register');
-  require('./test/specs/spec_helper');
+  require(PATHS.tests+'/spec_helper');
 
-  gulp.src(['test/specs/**/*.{coffee,js}'], { read: false })
-    .pipe(mocha({reporter: 'spec',timeout: 2000}));
+  gulp.src([PATHS.tests+'/**/*.{coffee,js}'], {read: false})
+    .pipe(plumber())
+    .pipe(mocha({reporter: 'spec',timeout: 200}));
 });
-
-gulp.task('test', ['mocha']);
 
 
 gulp.task('lint', function () {
-  gulp.src('./client/src/**/*.coffee')
+  gulp.src(PATHS.client+'/src/**/*.coffee')
       .pipe(watch())
-      .pipe(coffeelint())
-      .pipe(coffeelint.reporter('default'));
-
+      .pipe(coffeelint());
 });
 
 gulp.task('browserify_recorder', function () {
-  var bundler = watchify(['./client/src/bootstrap_recorder.coffee']);
+  var bundler = watchify([PATHS.client + '/src/bootstrap_recorder.coffee']);
   bundler.transform('coffeeify');
   bundler.on('update', rebundle);
 
@@ -65,20 +69,16 @@ gulp.task('watch', function () {
     'lib/**/*.*',
     'test/**/*.*',
     'client/**/*.*'
-  ], function () {
-    gulp.run('mocha');
-  });
+  ], [
+    'mocha'
+  ]);
 
-  gulp.watch([
-    'app/assets/css/**/*.less'
-  ], function(){
-    gulp.run('css');
-  });
+  gulp.watch([PATHS.css + '/*.less'], ['css']);
 });
 
 
 gulp.task('browserify_replayer', function () {
-  var bundler = watchify('./client/src/bootstrap_replayer.coffee');
+  var bundler = watchify(PATHS.client+'/src/bootstrap_replayer.coffee');
   bundler.transform('coffeeify');
   bundler.on('update', rebundle);
 
@@ -94,3 +94,4 @@ gulp.task('browserify_replayer', function () {
 
 
 gulp.task('default', ['lint', 'browserify_replayer', 'browserify_recorder', 'css', 'develop', 'watch'], function(){});
+gulp.task('test', ['mocha', 'watch']);
